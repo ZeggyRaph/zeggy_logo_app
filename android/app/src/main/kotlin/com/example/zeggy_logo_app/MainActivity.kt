34091c786e8deef66cfd01398b31bc7e3daa3ec3
RaphTech-Solutions <famouszegenee@gmail.com>
2022-04-14 17:41:24 +0100

    first commit of zeggy logo app

---
+++ android/app/src/main/kotlin/com/example/zeggy_logo_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.zeggy_logo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    home: Scaffold(
+        appBar: AppBar(
+      title: Text('Zeggy Logo App'),
+      backgroundColor: Colors.purple[900],
+    )),
+  ));
+}
