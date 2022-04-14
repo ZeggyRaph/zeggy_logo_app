d8e56316097be7ca2179e42d550b89f43d84063f
RaphTech-Solutions <famouszegenee@gmail.com>
2022-04-14 19:03:15 +0100

    adding image using NetworkImage widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  runApp(MaterialApp(
-    home: Scaffold(
+  runApp(
+    MaterialApp(
+      home: Scaffold(
         appBar: AppBar(
-      title: Text('Zeggy Logo App'),
-      backgroundColor: Colors.purple[900],
-    )),
-  ));
+          title: Text('Zeggy Logo App'),
+          backgroundColor: Colors.purple[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'),
+          ),
+        ),
+        backgroundColor: Colors.black,
+      ),
+    ),
+  );
 }
